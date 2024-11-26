da6c30cf3bf2536b52090e2b484d069732646845
vitor31309 <vitorcanto33@icloud.com>
2024-11-26 16:08:36 -0300

    modulo imagem

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -4,6 +4,8 @@ import { UserService } from '../user.service';
 import { Storage } from '@ionic/storage-angular';
 
 import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
+
+//import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 @Component({
   selector: 'app-tab3',
   templateUrl: './tab3.page.html',
@@ -11,6 +13,8 @@ import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
 })
 export class Tab3Page implements OnInit {
   user: any;
+
+  //avatarImage: string = 'assets/avatar-placeholder.png';
 
   constructor(private userService: UserService, private storage: Storage) {
     this.storage.create();  
@@ -26,4 +30,18 @@ export class Tab3Page implements OnInit {
 
   async ngOnInit() { const userData = await this.storage.get('user'); if (userData) { this.user = JSON.parse(userData); } else { this.user = null; } }
 
+  //selectAvatar() {
+   // const options: CameraOptions = {
+     // quality: 100,
+     // destinationType: this.camera.DestinationType.DATA_URL,
+     // encodingType: this.camera.EncodingType.JPEG,
+      //mediaType: this.camera.MediaType.PICTURE
+   //};
+
+    //this.camera.getPicture(options).then((imageData) => {
+      //this.avatarImage = 'data:image/jpeg;base64,' + imageData;
+    //}, (err) => {
+      //console.log("Error: " + err);
+    //});
+  //}
 }
